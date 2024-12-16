b86afe04f1360846cc8fb1a6d7ff6451e5008ece
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2024-12-16 09:41:33 +0430

    Add some section to this sit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Change Cards Bg colors
+
 const cards = document.querySelectorAll(".card-item");
 const firstImages = [
   "/imgs/service-icon-01.png",
@@ -22,4 +24,31 @@ cards.forEach((card, index) => {
     const img = card.querySelector("img");
     img.src = firstImages[index];
   });
+});
+
+// Change navbar color when thats scrolled
+
+window.onscroll = function () {
+  var navbar = document.querySelector(".navbar");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+};
+
+// Change Clients details
+
+let clientDetails = document.getElementsByClassName("clientDetails");
+let clientPara = document.getElementsByClassName("clientPara");
+
+let clientParagraphs = [
+  "“CTO, Lorem ipsum dolor sit amet, consectetur adpiscing elit, sed do eismod tempor idunte ut labore et dolore magna aliqua darwin kengan lorem ipsum dolor sit amet, consectetur picing elit massive big blasta.”",
+  "“May, Lorem ipsum dolor sit amet, consectetur adpiscing elit, sed do eismod tempor idunte ut labore et dolore magna aliqua darwin kengan lorem ipsum dolor sit amet, consectetur picing elit massive big blasta.”",
+  "“Lorem ipsum dolor sit amet, consectetur adpiscing elit, sed do eismod tempor idunte ut labore et dolore magna aliqua darwin kengan lorem ipsum dolor sit amet, consectetur picing elit massive big blasta.”",
+  "“Mark, Lorem ipsum dolor sit amet, consectetur adpiscing elit, sed do eismod tempor idunte ut labore et dolore magna aliqua darwin kengan lorem ipsum dolor sit amet, consectetur picing elit massive big blasta.”",
+];
+
+clientDetails.addEventListener("click", () => {
+  clientDetails.innerHtml = clientParagraphs[0];
 });
